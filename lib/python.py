b10611cb187673a67ab7b45e166ccec7db72beb7
victor jouin <77397820+victorjouin@users.noreply.github.com>
2023-05-14 14:48:18 +0200

    un putain de cauchemar

--- lib/python.py
+++ lib/python.py
@@ -8,28 +8,27 @@ import sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Append the path to the DLL to the system's search path
-dll_path = os.path.join(current_dir, 'ClassLin.dll')
+dll_path = os.path.join(current_dir, 'LinearCLassification')
 sys.path.append(dll_path)
 
 # Load the DLL
 dll = ctypes.CDLL(dll_path)
 
 # Définition des types de données d'entrée et de sortie de la fonction
-dll.linear_classification_export.argtypes = [ctypes.POINTER(ctypes.POINTER(ctypes.c_double)), ctypes.POINTER(ctypes.c_int), ctypes.c_double, ctypes.c_int, ctypes.c_char_p]
-dll.linear_classification_export.restype = ctypes.POINTER(ctypes.c_double)
+dll.linear_classification.argtypes = [ctypes.POINTER(ctypes.POINTER(ctypes.c_double)), ctypes.POINTER(ctypes.c_int), ctypes.c_double, ctypes.c_int]
+dll.linear_classification.restype = ctypes.POINTER(ctypes.c_double)
 
 # Préparation des données
 X = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]
 Y = [1, -1, 1]
 X_array = (ctypes.POINTER(ctypes.c_double) * len(X))(*[ctypes.cast((ctypes.POINTER(ctypes.c_double) * len(x))(*[ctypes.pointer(ctypes.c_double(i)) for i in x]), ctypes.POINTER(ctypes.c_double)) for x in X])
-
 Y_array = (ctypes.c_int * len(Y))(*Y)
 learning_rate = 0.01
 max_iterations = 1000
-filename = 'resultats.txt'.encode('utf-8')
+
 
 # Appel de la fonction
-resultat = dll.linear_classification_export(X_array, Y_array, learning_rate, max_iterations, filename)
+resultat = dll.linear_classification(X_array, Y_array, learning_rate, max_iterations)
 
 # Conversion des résultats en un tableau Python
 resultat = ctypes.cast(resultat, ctypes.POINTER(ctypes.c_double * (len(X[0]) + 1)))
