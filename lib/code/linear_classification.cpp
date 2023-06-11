279336d466247d229f759a35783512d2df07e3dc
victor jouin <77397820+victorjouin@users.noreply.github.com>
2023-06-11 16:28:21 +0200

    PLZ corrige cette merde

--- lib/code/linear_classification.cpp
+++ lib/code/linear_classification.cpp
@@ -1,76 +1,86 @@
 #include <iostream>
-#include <vector>
+#include <numeric>
+#include <algorithm>
 #include <cmath>
+#include <vector>
 #include <cstdlib>
 #include <ctime>
-#include <algorithm>
 
 extern "C" {
 
-void rosenblatt(double *X_data, double *Y_data, int rows, int cols, double learning_rate, int max_iterations, int nb_classes, double *W) {
-    // Vérification que les arguments en entrée sont valides
-    if (rows <= 0 || cols <= 0 || nb_classes <= 0) {
-        std::cerr << "Erreur : les dimensions des données et des poids doivent être strictement positives" << std::endl;
-        return;
+void rosenblatt(double *X_data, double *Y_data, int rows, int cols, double learning_rate, int max_iterations, int nb_classes, double *W)
+{
+    // Initialisation du générateur de nombres aléatoires
+    srand(time(NULL));
+
+    // Ajout du biais à X
+    std::vector<double> X(rows * (cols + 1));
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            X[i * (cols + 1) + j] = X_data[i * cols + j];
+        }
+        X[i * (cols + 1) + cols] = 1.0;
     }
 
-    // Ajout d'un biais aux données
-    int nb_features = cols + 1;
-    std::vector<double> X(rows * nb_features);
-    for (int i = 0; i < rows; i++) {
-        X[i * nb_features] = 1.0;
-        for (int j = 1; j < nb_features; j++) {
-            X[i * nb_features + j] = X_data[i * cols + j - 1];
+    // Initialisation des poids avec des valeurs aléatoires centrées sur zéro et d'une variance égale à 1/cols
+    double variance = 1.0 / cols;
+    std::vector<std::vector<double>> W_vec(nb_classes, std::vector<double>(cols + 1));
+    for (int c = 0; c < nb_classes; c++)
+    {
+        for (int j = 0; j <= cols; j++)
+        {
+            W_vec[c][j] = variance * ((double) rand() / RAND_MAX - 0.5);
         }
     }
 
-    // Initialisation aléatoire des poids
-    std::srand(std::time(nullptr));
-    std::vector<double> weights(nb_features * nb_classes);
-    for (int i = 0; i < nb_features * nb_classes; i++) {
-        weights[i] = ((double) std::rand() / RAND_MAX) - 0.5;
-    }
+    // Entraînement d'un perceptron pour chaque classe
+    int nb_iterations = 0;
+    for (int c = 0; c < nb_classes; c++)
+    {
+        double error = 1.0;
+        while (error > 0.01 && nb_iterations < max_iterations)
+        {
+            // Générer un nombre aléatoire pour sélectionner une observation au hasard
+            int random_idx = rand() % rows;
+            double *x = &X[random_idx * (cols + 1)];
+            double y = (Y_data[random_idx] == c) ? 1 : -1; // 1 si l'observation appartient à la classe c, -1 sinon
+            double dot_product = std::inner_product(x, x + cols + 1, W_vec[c].begin(), 0.0);
 
-    // Boucle d'apprentissage
-    int t;
-    for (t = 0; t < max_iterations; t++) {
-        // Tirage aléatoire d'un exemple
-        int i = std::rand() % rows;
-
-        // Calcul des scores pour chaque classe
-        std::vector<double> scores(nb_classes);
-        for (int c = 0; c < nb_classes; c++) {
-            double score = 0.0;
-            for (int j = 0; j < nb_features; j++) {
-                score += X[i * nb_features + j] * weights[c * nb_features + j];
+            // Mettre à jour les poids en fonction de l'observation sélectionnée aléatoirement
+            if (y * dot_product < 0)
+            {
+                for (int j = 0; j <= cols; j++)
+                {
+                    W_vec[c][j] += learning_rate * y * x[j];
+                }
             }
-            scores[c] = score;
-        }
 
-        // Calcul de la classe prédite
-        int y_pred = 0;
-        double max_score = scores[0];
-        for (int c = 0; c < nb_classes; c++) {
-            if (scores[c] > max_score) {
-                y_pred = c;
-                max_score = scores[c];
+            // Calculer l'erreur
+            error = 0.0;
+            for (int i = 0; i < rows; i++)
+            {
+                double *x_i = &X[i * (cols + 1)];
+                double y_i = (Y_data[i] == c) ? 1 : -1;
+                double dot_product_i = std::inner_product(x_i, x_i + cols + 1, W_vec[c].begin(), 0.0);
+                error += std::max(0.0, -y_i * dot_product_i);
             }
-        }
-
-        // Mise à jour des poids si la prédiction est incorrecte
-        if (y_pred != (int) Y_data[i]) {
-            int y_true = (int) Y_data[i];
-            for (int j = 0; j < nb_features; j++) {
-                weights[y_true * nb_features + j] += learning_rate * X[i * nb_features + j];
-                weights[y_pred * nb_features + j] -= learning_rate * X[i * nb_features + j];
-            }
+            error /= rows;
+            nb_iterations++;
         }
     }
 
-    std::cout << "Nombre d'iterations : " << t << std::endl;
+    // Les poids sont stockés dans la matrice W pour Python
+    for (int c = 0; c < nb_classes; c++)
+    {
+        for (int j = 0; j <= cols; j++)
+        {
+            W[c * (cols + 1) + j] = W_vec[c][j];
+        }
+    }
 
-    // Copie des poids dans le tableau de sortie
-    std::copy(weights.begin(), weights.end(), W);
+    printf("Nombre d'itérations effectuées : %d\n", nb_iterations);
 }
 
 } // extern "C"
--- lib/lib_dll/test_linear_classification.py
+++ lib/lib_dll/test_linear_classification.py
@@ -19,8 +19,7 @@ dll.rosenblatt.argtypes = [np.ctypeslib.ndpointer(dtype=np.double, ndim=2, flags
 
 # Exemple d'utilisation
 X = np.random.random((500, 2)) * 2.0 - 1.0
-X_bias = np.ones((X.shape[0], 1))
-X_ctype = np.hstack((X_bias, X)).astype(np.float64)
+X_ctype = X.astype(np.float64)
 Y = np.array([[1, 0, 0] if -p[0] - p[1] - 0.5 > 0 and p[1] < 0 and p[0] - p[1] - 0.5 < 0 else 
               [0, 1, 0] if -p[0] - p[1] - 0.5 < 0 and p[1] > 0 and p[0] - p[1] - 0.5 < 0 else 
               [0, 0, 1] if -p[0] - p[1] - 0.5 < 0 and p[1] < 0 and p[0] - p[1] - 0.5 > 0 else 
@@ -29,32 +28,39 @@ Y_ctype = np.array([[1, 0, 0] if -p[0] - p[1] - 0.5 > 0 and p[1] < 0 and p[0] - 
               [0, 1, 0] if -p[0] - p[1] - 0.5 < 0 and p[1] > 0 and p[0] - p[1] - 0.5 < 0 else 
               [0, 0, 1] if -p[0] - p[1] - 0.5 < 0 and p[1] < 0 and p[0] - p[1] - 0.5 > 0 else 
               [0, 0, 0]for p in X], dtype=np.float64).flatten()
-learning_rate = 0.01
-max_iterations = 50000
+learning_rate = 0.00004
+max_iterations = 5000000
 nb_classes = 3
-W = np.random.normal(0, 1/np.sqrt(X_ctype.shape[1]), size=(X_ctype.shape[1], nb_classes))
+W = np.random.random((X.shape[1] + 1) * nb_classes) - 0.5
 
 
-
-dll.rosenblatt(X_ctype, Y_ctype, X_ctype.shape[0], X_ctype.shape[1], learning_rate, max_iterations, nb_classes, W.flatten())
-
+dll.rosenblatt(X_ctype, Y_ctype, X_ctype.shape[0], X.shape[1], learning_rate, max_iterations, nb_classes, W)
 
 # Afficher les poids pour chaque perceptron
+W = W.reshape((X.shape[1] + 1, nb_classes))
 print(W)
+
+
+# Prédiction de la classe pour chaque observation
+predictions = np.argmax(np.dot(np.concatenate([X, np.ones((X.shape[0], 1))], axis=1), W), axis=1)
 
 # Affichage des données
 plt.scatter(np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][0] == 1, enumerate(X)))))[:,0], np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][0] == 1, enumerate(X)))))[:,1], color='blue')
 plt.scatter(np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][1] == 1, enumerate(X)))))[:,0], np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][1] == 1, enumerate(X)))))[:,1], color='red')
 plt.scatter(np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][2] == 1, enumerate(X)))))[:,0], np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][2] == 1, enumerate(X)))))[:,1], color='green')
 
-
 # Affichage de la droite de séparation pour chaque perceptron
-x = np.linspace(-1.5, 1.5, 100)
-colors = ['blue', 'red', 'green']
-for c in range(nb_classes):
+x = np.linspace(-1.5, 1.5, 1000000)
+c = 0
+w = W[:-1, c]
+b = W[-1, c]
+x_min, x_max = plt.xlim()
+x_values = np.linspace(x_min, x_max, 2)
+while c < nb_classes: 
     w = W[:-1, c]
     b = W[-1, c]
-    y = -(w[0] * x + b) / (w[1] + 1e-10)
-    plt.plot(x, y, color=colors[c])
+    y_values = -(w[1] / w[0]) * (x_values - b / w[0])
+    plt.plot(x_values, y_values)
+    c += 1
 
 plt.show()
