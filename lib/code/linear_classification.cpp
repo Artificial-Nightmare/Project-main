b07dd86b60852d05d7f66b89e5afb61bbc14b8d2
victor jouin <77397820+victorjouin@users.noreply.github.com>
2023-06-11 14:01:50 +0200

    c'est nul j'ai besoin d'aide

--- lib/code/linear_classification.cpp
+++ lib/code/linear_classification.cpp
@@ -1,74 +1,76 @@
 #include <iostream>
-#include <numeric>
-#include <algorithm>
-#include <cmath>
 #include <vector>
+#include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
 
 extern "C" {
 
-void rosenblatt(double *X_data, double *Y_data, int rows, int cols, double learning_rate, int max_iterations, int nb_classes, double *W)
-{
-    // Initialisation du générateur de nombres aléatoires
-    srand(time(NULL));
-
-    // Ajout du biais à X
-    std::vector<double> X(rows * (cols + 1));
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            X[i * (cols + 1) + j] = X_data[i * cols + j];
-        }
-        X[i * (cols + 1) + cols] = 1.0;
+void rosenblatt(double *X_data, double *Y_data, int rows, int cols, double learning_rate, int max_iterations, int nb_classes, double *W) {
+    // Vérification que les arguments en entrée sont valides
+    if (rows <= 0 || cols <= 0 || nb_classes <= 0) {
+        std::cerr << "Erreur : les dimensions des données et des poids doivent être strictement positives" << std::endl;
+        return;
     }
 
-    // Initialisation des poids avec des valeurs aléatoires centrées sur zéro et d'une variance égale à 1/cols
-    double variance = 1.0 / cols;
-    std::vector<double> W_vec(nb_classes * (cols + 1));
-    for (int i = 0; i < nb_classes; i++)
-    {
-        for (int j = 0; j <= cols; j++)
-        {
-            W_vec[i * (cols + 1) + j] = variance * ((double) rand() / RAND_MAX - 0.5);
+    // Ajout d'un biais aux données
+    int nb_features = cols + 1;
+    std::vector<double> X(rows * nb_features);
+    for (int i = 0; i < rows; i++) {
+        X[i * nb_features] = 1.0;
+        for (int j = 1; j < nb_features; j++) {
+            X[i * nb_features + j] = X_data[i * cols + j - 1];
         }
     }
 
-    // Entraînement d'un perceptron pour chaque classe
-    for (int c = 0; c < nb_classes; c++)
-    {
-        bool misclassified = true;
-        for (int i = 0; i < max_iterations && misclassified; i++)
-        {
-            // Générer un nombre aléatoire pour sélectionner une observation au hasard
-            int random_idx = rand() % rows;
-            double *x = &X[random_idx * (cols + 1)];
-            double y = (Y_data[random_idx] == c) ? 1 : -1; // 1 si l'observation appartient à la classe c, -1 sinon
-            double dot_product = std::inner_product(x, x + cols + 1, &W_vec[c * (cols + 1)], 0.0);
-            
-            // Mettre à jour les poids en fonction de l'observation sélectionnée aléatoirement
-            if (y * dot_product < 0)
-            {
-                for (int j = 0; j <= cols; j++)
-                {
-                    W_vec[c * (cols + 1) + j] += learning_rate * y * x[j];
-                }
-                misclassified = true;
+    // Initialisation aléatoire des poids
+    std::srand(std::time(nullptr));
+    std::vector<double> weights(nb_features * nb_classes);
+    for (int i = 0; i < nb_features * nb_classes; i++) {
+        weights[i] = ((double) std::rand() / RAND_MAX) - 0.5;
+    }
+
+    // Boucle d'apprentissage
+    int t;
+    for (t = 0; t < max_iterations; t++) {
+        // Tirage aléatoire d'un exemple
+        int i = std::rand() % rows;
+
+        // Calcul des scores pour chaque classe
+        std::vector<double> scores(nb_classes);
+        for (int c = 0; c < nb_classes; c++) {
+            double score = 0.0;
+            for (int j = 0; j < nb_features; j++) {
+                score += X[i * nb_features + j] * weights[c * nb_features + j];
             }
-            else
-            {
-                misclassified = false;
+            scores[c] = score;
+        }
+
+        // Calcul de la classe prédite
+        int y_pred = 0;
+        double max_score = scores[0];
+        for (int c = 0; c < nb_classes; c++) {
+            if (scores[c] > max_score) {
+                y_pred = c;
+                max_score = scores[c];
             }
         }
+
+        // Mise à jour des poids si la prédiction est incorrecte
+        if (y_pred != (int) Y_data[i]) {
+            int y_true = (int) Y_data[i];
+            for (int j = 0; j < nb_features; j++) {
+                weights[y_true * nb_features + j] += learning_rate * X[i * nb_features + j];
+                weights[y_pred * nb_features + j] -= learning_rate * X[i * nb_features + j];
+            }
+        }
     }
 
-    // Les poids sont stockés dans la matrice W pour Python
-    for (int i = 0; i < nb_classes * (cols + 1); i++)
-    {
-        W[i] = W_vec[i];
-    }
+    std::cout << "Nombre d'iterations : " << t << std::endl;
+
+    // Copie des poids dans le tableau de sortie
+    std::copy(weights.begin(), weights.end(), W);
 }
-
 
 } // extern "C"
