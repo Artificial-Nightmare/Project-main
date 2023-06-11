6383febf8b2ebc62d24b9f7a040d026075b6dde7
victor jouin <77397820+victorjouin@users.noreply.github.com>
2023-06-11 02:24:09 +0200

    good luck I'm out

--- lib/code/linear_classification.cpp
+++ lib/code/linear_classification.cpp
@@ -3,10 +3,16 @@
 #include <algorithm>
 #include <cmath>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+
 extern "C" {
 
-void rosenblatt(double *X_data, double *Y_data, int rows, int cols, double learning_rate, int max_iterations, double *w)
+void rosenblatt(double *X_data, double *Y_data, int rows, int cols, double learning_rate, int max_iterations, int nb_classes, double *W)
 {
+    // Initialisation du générateur de nombres aléatoires
+    srand(time(NULL));
+
     // Ajout du biais à X
     std::vector<double> X(rows * (cols + 1));
     for (int i = 0; i < rows; i++)
@@ -18,44 +24,51 @@ void rosenblatt(double *X_data, double *Y_data, int rows, int cols, double learn
         X[i * (cols + 1) + cols] = 1.0;
     }
 
-    // Initialisation des poids à zéro c'est un vecteur de taille cols + 1 car on a ajouté le biais à X 
-    std::vector<double> w_vec(cols + 1);
-for (int i = 0; i <= cols; i++)
-{
-    w_vec[i] = rand() / double(RAND_MAX);
-} // random ou 0 ?
-    // Entraînement du perceptron pendant max_iterations itérations
-    for (int i = 0; i < max_iterations; i++)
+    // Initialisation des poids avec des valeurs aléatoires centrées sur zéro et d'une variance égale à 1/cols
+    double variance = 1.0 / cols;
+    std::vector<double> W_vec(nb_classes * (cols + 1));
+    for (int i = 0; i < nb_classes; i++)
     {
-        bool misclassified = false;
-        for (int j = 0; j < rows; j++)
+        for (int j = 0; j <= cols; j++)
         {
-            double *x = &X[j * (cols + 1)];
-            double y = Y_data[j];
-            double dot_product = std::inner_product(x, x + cols + 1, w_vec.begin(), 0.0);
-            if (y * dot_product <= 0)
-            {
-                misclassified = true;
-                w_vec[0] += learning_rate * y;
-                for (int k = 1; k <= cols; k++)
-                {
-                    w_vec[k] += learning_rate * y * x[k];
-                }
-            }
+            W_vec[i * (cols + 1) + j] = variance * ((double) rand() / RAND_MAX - 0.5);
         }
-        if (!misclassified)
-        {
-            break;
-        }
-        // Vérifier si la droite de séparation a le bon sens et inverser les poids si nécessaire
     }
 
-    // les poids sortie w (pour Python) 
-    for (int i = 0; i <= cols; i++)
+    // Entraînement d'un perceptron pour chaque classe
+    for (int c = 0; c < nb_classes; c++)
     {
-        w[i] = w_vec[i];
+        bool misclassified = true;
+        for (int i = 0; i < max_iterations && misclassified; i++)
+        {
+            // Générer un nombre aléatoire pour sélectionner une observation au hasard
+            int random_idx = rand() % rows;
+            double *x = &X[random_idx * (cols + 1)];
+            double y = (Y_data[random_idx] == c) ? 1 : -1; // 1 si l'observation appartient à la classe c, -1 sinon
+            double dot_product = std::inner_product(x, x + cols + 1, &W_vec[c * (cols + 1)], 0.0);
+            
+            // Mettre à jour les poids en fonction de l'observation sélectionnée aléatoirement
+            if (y * dot_product < 0)
+            {
+                for (int j = 0; j <= cols; j++)
+                {
+                    W_vec[c * (cols + 1) + j] += learning_rate * y * x[j];
+                }
+                misclassified = true;
+            }
+            else
+            {
+                misclassified = false;
+            }
+        }
+    }
+
+    // Les poids sont stockés dans la matrice W pour Python
+    for (int i = 0; i < nb_classes * (cols + 1); i++)
+    {
+        W[i] = W_vec[i];
     }
 }
 
 
-}  // extern "C"
+} // extern "C"
--- lib/lib_dll/test_linear_classification.py
+++ lib/lib_dll/test_linear_classification.py
@@ -1,48 +1,62 @@
 import ctypes
 import numpy as np
 import os
-import sys
 import matplotlib.pyplot as plt
 
-# Get the directory of the current script
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Append the path to the DLL to the system's search path
-dll_path = os.path.join(current_dir, 'liblinear_classification.dll')
-sys.path.append(dll_path)
+# Charger la DLL contenant la fonction rosenblatt
+dll_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'liblinear_classification.dll')
 dll = ctypes.cdll.LoadLibrary(dll_path)
 
-# Définition des types de données pour la fonction rosenblatt
+# Définir les types de données pour la fonction rosenblatt
 dll.rosenblatt.argtypes = [np.ctypeslib.ndpointer(dtype=np.double, ndim=2, flags='C_CONTIGUOUS'),
                            np.ctypeslib.ndpointer(dtype=np.double, ndim=1, flags='C_CONTIGUOUS'),
-                           ctypes.c_int, ctypes.c_int, ctypes.c_double, ctypes.c_int,
+                           ctypes.c_int,
+                           ctypes.c_int,
+                           ctypes.c_double,
+                           ctypes.c_int,
+                           ctypes.c_int,
                            np.ctypeslib.ndpointer(dtype=np.double, ndim=1, flags='C_CONTIGUOUS')]
 
 # Exemple d'utilisation
-# pas oublier de mettre le dtype=np.float64 !
-X = np.concatenate([np.random.random((50,2)) * 1.1 + np.array([1, 1]), np.random.random((50,2)) * 1.1 + np.array([2, 2])], dtype=np.float64)
-Y = np.concatenate([np.ones((50, 1)), np.ones((50, 1)) * -1.0], dtype=np.float64).flatten()
-learning_rate = 0.1
-max_iterations = 100
-# Centrage et réduction des données
-X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+X = np.random.random((500, 2)) * 2.0 - 1.0
+X_ctype = X.astype(np.float64)
+Y = np.array([[1, 0, 0] if -p[0] - p[1] - 0.5 > 0 and p[1] < 0 and p[0] - p[1] - 0.5 < 0 else 
+              [0, 1, 0] if -p[0] - p[1] - 0.5 < 0 and p[1] > 0 and p[0] - p[1] - 0.5 < 0 else 
+              [0, 0, 1] if -p[0] - p[1] - 0.5 < 0 and p[1] < 0 and p[0] - p[1] - 0.5 > 0 else 
+              [0, 0, 0]for p in X], dtype=np.float64)
+Y_ctype = np.array([[1, 0, 0] if -p[0] - p[1] - 0.5 > 0 and p[1] < 0 and p[0] - p[1] - 0.5 < 0 else 
+              [0, 1, 0] if -p[0] - p[1] - 0.5 < 0 and p[1] > 0 and p[0] - p[1] - 0.5 < 0 else 
+              [0, 0, 1] if -p[0] - p[1] - 0.5 < 0 and p[1] < 0 and p[0] - p[1] - 0.5 > 0 else 
+              [0, 0, 0]for p in X], dtype=np.float64).flatten()
+learning_rate = 0.001
+max_iterations = 100000
+nb_classes = 3
+W = np.random.random((X.shape[1] + 1) * nb_classes) - 0.5
 
-# Créer un tableau de sortie pour les poids entraînés
-w = np.zeros(X.shape[1] + 1, dtype=np.float64)
 
-# Appeler la fonction rosenblatt avec un pointeur vers le tableau de sortie
-dll.rosenblatt(X, Y, X.shape[0], X.shape[1], learning_rate, max_iterations, w)
+dll.rosenblatt(X_ctype, Y_ctype, X_ctype.shape[0], X.shape[1], learning_rate, max_iterations, nb_classes, W)
+
+# Afficher les poids pour chaque perceptron
+W = W.reshape((X.shape[1] + 1, nb_classes))
+print(W)
 
 
-# Affichage des poids
-print(w)
+# Prédiction de la classe pour chaque observation
+predictions = np.argmax(np.dot(np.concatenate([X, np.ones((X.shape[0], 1))], axis=1), W), axis=1)
+
 # Affichage des données
-plt.scatter(X[0:50, 0], X[0:50, 1], color='blue')
-plt.scatter(X[50:100,0], X[50:100,1], color='red') 
-# Affichage de la droite de séparation
-x = np.linspace(-2, 2, 100)
-y = -(w[0] * x + w[2]) / w[1]
-plt.plot(x, y, color='black')
+plt.scatter(np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][0] == 1, enumerate(X)))))[:,0], np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][0] == 1, enumerate(X)))))[:,1], color='blue')
+plt.scatter(np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][1] == 1, enumerate(X)))))[:,0], np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][1] == 1, enumerate(X)))))[:,1], color='red')
+plt.scatter(np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][2] == 1, enumerate(X)))))[:,0], np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]][2] == 1, enumerate(X)))))[:,1], color='green')
+
+# Affichage de la droite de séparation pour chaque perceptron
+x = np.linspace(-1.5, 1.5, 1000000)
+c = 0
+while c < nb_classes:
+    w = W[:-1, c]
+    b = W[-1, c]
+    y = (w[0] * x + b) / w[1]
+    plt.plot(x, y)
+    c += 1
+
 plt.show()
-
-
