a7704a7c26aaf42698ccc24bda021d47b957a1a1
victor jouin <77397820+victorjouin@users.noreply.github.com>
2023-07-10 10:45:34 +0200

    SVM seems working

--- lib/code/SVM.cpp
+++ lib/code/SVM.cpp
@@ -1,58 +1,101 @@
-#include <cmath>
 #include <iostream>
 
 extern "C" {
 
-    void trainSVM(const double* trainingData, size_t numExamples, size_t numFeatures, double* weights, double* bias, int numIterations) {
-        // Initialiser les poids et le biais à zéro
-        for (size_t i = 0; i < numFeatures; i++) {
-            weights[i] = 0.0;
-        }
-        *bias = 0.0;
 
-        // Boucle d'entraînement
-        for (int iteration = 0; iteration < numIterations; iteration++) {
-            double totalLoss = 0.0;
-            int correctCount = 0;
+// Structure pour stocker les exemples d'entraînement
+struct TrainingExample {
+    double* features;
+    int label;
+};
 
-            for (size_t i = 0; i < numExamples; i++) {
-                const double* example = trainingData + i * (numFeatures + 1);
-                double prediction = *bias;
+// Fonction pour entraîner le SVM avec l'algorithme du perceptron
+void trainSVM(double* trainingData, size_t numExamples, size_t numFeatures, double* weights, double& bias, int numIterations) {
+    // Initialiser les poids et le biais à zéro
+    for (size_t i = 0; i < numFeatures; i++) {
+        weights[i] = 0.0;
+    }
+    bias = 0.0;
 
+    // Boucle d'entraînement
+    for (int iteration = 0; iteration < numIterations; iteration++) {
+        int misclassifiedCount = 0;
+
+        for (size_t i = 0; i < numExamples; i++) {
+            double* example = trainingData + i * (numFeatures + 1);
+            double prediction = bias;
+
+            for (size_t j = 0; j < numFeatures; j++) {
+                prediction += weights[j] * example[j];
+            }
+
+            // Mettre à jour les poids et le biais en cas d'erreur de classification
+            if (example[numFeatures] * prediction <= 0) {
                 for (size_t j = 0; j < numFeatures; j++) {
-                    prediction += weights[j] * example[j];
+                    weights[j] += example[j] * example[numFeatures];
                 }
-
-                double loss = example[numFeatures] * prediction;
-
-                // Mettre à jour les poids et le biais en fonction de la perte
-                if (loss <= 0) {
-                    for (size_t j = 0; j < numFeatures; j++) {
-                        weights[j] += example[j] * example[numFeatures];
-                    }
-                    *bias += example[numFeatures];
-                } else {
-                    correctCount++;
-                }
-
-                totalLoss += std::abs(loss);
-            }
-
-            if (iteration % 500 == 0) {
-                double accuracy = static_cast<double>(correctCount) / numExamples * 100.0;
-                std::cout << "Iteration: " << iteration << " Loss: " << totalLoss << " Accuracy: " << accuracy << "%" << std::endl;
+                bias += example[numFeatures];
+                misclassifiedCount++;
             }
         }
-    }
 
-    // Fonction pour prédire le label avec le SVM entraîné
-    int predictSVM(const double* features, const double* weights, double bias, size_t numFeatures) {
-        double prediction = bias;
+        // Affichage de l'évolution de l'entraînement
+        std::cout << "Iteration: " << iteration << " Misclassified Count: " << misclassifiedCount << std::endl;
 
-        for (size_t i = 0; i < numFeatures; i++) {
-            prediction += weights[i] * features[i];
+        // Arrêter l'entraînement si tous les exemples sont correctement classés
+        if (misclassifiedCount == 0) {
+            break;
         }
-
-        return (prediction >= 0) ? 1 : -1;
     }
+}
+
+// Fonction pour prédire la classe d'un exemple avec le SVM entraîné
+int predictSVM(const double* features, const double* weights, double bias, size_t numFeatures) {
+    double prediction = bias;
+
+    for (size_t i = 0; i < numFeatures; i++) {
+        prediction += weights[i] * features[i];
+    }
+
+    return (prediction >= 0) ? 1 : -1;
+}
+
+int main() {
+    // Création des données d'entraînement pour un test difficile de classification linéaire
+    double trainingData[] = {
+        0, 0, -1,
+        0, 1, -1,
+        1, 0, -1,
+        1, 1, -1,
+        0.5, 0.5, 1,
+        1.5, 1.5, 1
+    };
+
+    size_t numExamples = 6;
+    size_t numFeatures = 2;
+
+    // Entraînement du SVM
+    double weights[numFeatures];
+    double bias;
+    int numIterations = 1000;
+
+    trainSVM(trainingData, numExamples, numFeatures, weights, bias, numIterations);
+
+    // Création des données de test
+    double testData[] = {
+        0.2, 0.2,
+        1.2, 1.2
+    };
+
+    size_t numTestExamples = 2;
+
+    // Affichage des prédictions pour les données de test
+    for (size_t i = 0; i < numTestExamples; i++) {
+        const double* features = testData + i * numFeatures;
+        int prediction = predictSVM(features, weights, bias, numFeatures);
+        std::cout << "Features: " << features[0] << ", " << features[1] << " => Output: " << prediction << std::endl;
+    }
+
+    return 0;
+}
 }
--- lib/lib_dll/SVM.py
+++ lib/lib_dll/SVM.py
@@ -2,6 +2,7 @@ import ctypes
 import numpy as np
 import os
 import sys
+import matplotlib.pyplot as plt
 
 # Get the directory of the current script
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -11,57 +12,82 @@ dll_path = os.path.join(current_dir, 'SVM.dll')
 sys.path.append(dll_path)
 
 # Load the DLL
-svm_dll = ctypes.cdll.LoadLibrary(dll_path)
-
+svm_lib = ctypes.cdll.LoadLibrary(dll_path)
 
 # Définition des types de données pour les fonctions de la DLL
-svm_dll.trainSVM.argtypes = [np.ctypeslib.ndpointer(dtype=np.double, ndim=2, flags="C_CONTIGUOUS"),
-                             ctypes.c_size_t,
-                             ctypes.c_size_t,
-                             np.ctypeslib.ndpointer(dtype=np.double, ndim=1),
-                             ctypes.POINTER(ctypes.c_double),
-                             ctypes.c_int]
+svm_lib.trainSVM.argtypes = [
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags="C_CONTIGUOUS"),
+    ctypes.c_size_t,
+    ctypes.c_size_t,
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags="C_CONTIGUOUS"),
+    ctypes.POINTER(ctypes.c_double),
+    ctypes.c_int
+]
+svm_lib.trainSVM.restype = None
 
-svm_dll.predictSVM.restype = ctypes.c_int
-svm_dll.predictSVM.argtypes = [np.ctypeslib.ndpointer(dtype=np.double, ndim=1),
-                               np.ctypeslib.ndpointer(dtype=np.double, ndim=1),
-                               ctypes.c_double,
-                               ctypes.c_size_t]
+svm_lib.predictSVM.argtypes = [
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags="C_CONTIGUOUS"),
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=1, flags="C_CONTIGUOUS"),
+    ctypes.c_double,
+    ctypes.c_size_t
+]
+svm_lib.predictSVM.restype = ctypes.c_int
 
+# Fonction pour entraîner le SVM avec l'algorithme du perceptron
+def trainSVM(trainingData, numIterations):
+    # Extraire les exemples d'entraînement et les labels
+    features = trainingData[:, :-1].astype(np.float64)
+    labels = trainingData[:, -1].astype(np.float64)
 
-def create_training_data():
-    trainingData = np.array([
-        [0, 1, 1],
-        [1, 1, 1],
-        [1, 0, -1],
-        [0, 0, -1]
-    ], dtype=np.double)
+    # Initialiser les poids et le biais à zéro
+    weights = np.zeros(features.shape[1], dtype=np.float64)
+    bias = ctypes.c_double(0.0)
 
-    return trainingData
-
-# Entraînement du SVM
-def train_svm(trainingData):
-    numExamples, numFeatures = trainingData.shape[0], trainingData.shape[1] - 1
-    weights = np.zeros(numFeatures, dtype=np.double)
-    bias = ctypes.c_double()
-
-    svm_dll.trainSVM(trainingData, numExamples, numFeatures, weights, ctypes.byref(bias), 1000000)
+    # Appeler la fonction C++ pour entraîner le SVM
+    svm_lib.trainSVM(features, features.shape[0], features.shape[1], weights, ctypes.byref(bias), numIterations)
 
     return weights, bias.value
 
-# Prédiction avec le SVM entraîné
-def predict_svm(features, weights, bias):
-    prediction = svm_dll.predictSVM(features, weights, bias, len(features))
-    return prediction
+# Fonction pour prédire la classe d'un exemple avec le SVM entraîné
+def predictSVM(features, weights, bias):
+    features = [float(x) for x in features]
+    return svm_lib.predictSVM(np.array(features, dtype=np.float64), weights, bias, len(features))
 
-# Création des données d'entraînement pour la classification linéaire
-trainingData = create_training_data()
+
+# Création des données d'entraînement pour un test difficile de classification linéaire
+trainingData = np.array([
+     [1, 1,1],
+      [2, 3,-1],
+      [3, 3,-1]
+])
 
 # Entraînement du SVM
-weights, bias = train_svm(trainingData)
+numIterations = 1000
+weights, bias = trainSVM(trainingData, numIterations)
 
-# Affichage des prédictions pour toutes les données d'entraînement
-for example in trainingData:
-    features = example[:-1]
-    prediction = predict_svm(features, weights, bias)
-    print(f"Features: {features} => Output: {prediction}")
+# Création des données de test
+testData = np.array([
+    [-1.5, -1.5],
+    [1.5, 1.5]
+])
+
+# Affichage des prédictions pour les données de test
+for i in range(testData.shape[0]):
+    features = testData[i]
+    prediction = predictSVM(features, weights, bias)
+    print(f"Features: {features[0]}, {features[1]} => Output: {prediction}")
+
+# Tracer les données d'entraînement
+plt.scatter(trainingData[:, 0], trainingData[:, 1], c=trainingData[:, 2], cmap='bwr')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.title('Training Data')
+
+# Tracer les frontières de décision du SVM
+x_min, x_max = plt.xlim()
+y_min, y_max = plt.ylim()
+xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
+Z = np.array([predictSVM([float(x), float(y)], weights, bias) for x, y in zip(xx.ravel(), yy.ravel())], dtype=np.float64)
+Z = Z.reshape(xx.shape)
+plt.contour(xx, yy, Z, colors='black', linewidths=1)
+plt.show()
