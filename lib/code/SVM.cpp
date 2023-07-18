99d069c7c31a8cc75abb694227a62c4cb5b0552f
victorjouin <davidmoresta1@gmail.com>
2023-07-18 22:31:03 +0200

    SVM correction

--- lib/code/SVM.cpp
+++ lib/code/SVM.cpp
@@ -3,21 +3,19 @@
 extern "C" {
 
 
-// Structure pour stocker les exemples d'entraînement
 struct TrainingExample {
     double* features;
     int label;
 };
 
-// Fonction pour entraîner le SVM avec l'algorithme du perceptron
 void trainSVM(double* trainingData, size_t numExamples, size_t numFeatures, double* weights, double& bias, int numIterations) {
-    // Initialiser les poids et le biais à zéro
+    // Initialiser les poids
     for (size_t i = 0; i < numFeatures; i++) {
         weights[i] = 0.0;
     }
     bias = 0.0;
 
-    // Boucle d'entraînement
+    // entraînement
     for (int iteration = 0; iteration < numIterations; iteration++) {
         int misclassifiedCount = 0;
 
@@ -39,17 +37,16 @@ void trainSVM(double* trainingData, size_t numExamples, size_t numFeatures, doub
             }
         }
 
-        // Affichage de l'évolution de l'entraînement
+        // Affichage
         std::cout << "Iteration: " << iteration << " Misclassified Count: " << misclassifiedCount << std::endl;
 
-        // Arrêter l'entraînement si tous les exemples sont correctement classés
+        // stop si c propre
         if (misclassifiedCount == 0) {
             break;
         }
     }
 }
 
-// Fonction pour prédire la classe d'un exemple avec le SVM entraîné
 int predictSVM(const double* features, const double* weights, double bias, size_t numFeatures) {
     double prediction = bias;
 
